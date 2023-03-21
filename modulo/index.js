66870684d0b41a1fba1468e548c69269829e1568
Millena Ferreira <millenamills44@gmail.com>
2023-03-21 20:51:05 -0300

    endPoint feito

---
+++ modulo/index.js
@@ -0,0 +1,25 @@
+const jsonContatos = require('./contatos.js')
+
+const getTelefone = (telefone) => {
+
+    let PerfilJSON = {}
+    let status = false
+
+    jsonContatos.contatos["whats-users"].forEach(contatosDados => {
+        if (contatosDados.number == telefone) {
+            PerfilJSON.contacts = contatosDados.contacts
+            status = true
+        }
+    })
+
+    if (status == true) {
+        return PerfilJSON
+    } else {
+        return status
+    }
+    
+}
+
+module.exports = {
+    getTelefone
+}
